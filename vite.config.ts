d79cc3a9e4010f481b42b9570c96f5161dd16c78
Mamadou Diouf <mdiouf@softvalleylabs.com>
2025-02-20 14:09:51 +0100

    Mise à jour de la configuration Vite pour permettre l'hôte et le port dynamiques dans les scripts de prévisualisation et de serveur.

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+  server: {
+    host: true,                      
+    port: Number(process.env.PORT) || 5173
+  },
   preview: {
-    host: '0.0.0.0',
-    port: Number(process.env.PORT) || 10000, 
-    allowedHosts: ['stream-front-h5re.onrender.com']
+    host: true,
+    port: Number(process.env.PORT) || 5173
   }
 })
